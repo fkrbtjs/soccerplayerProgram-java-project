16ea7dabf4e7225fa8895a4e32d3a58d65c7456a
cisuarez <117554825+cisuarez@users.noreply.github.com>
2023-08-06 16:35:52 -0500

    Create PlayerDeleteOperation.java

---
+++ src/soccer/PlayerDeleteOperation.java
@@ -0,0 +1,7 @@
+public class PlayerDeleteOperation implements PlayerOperation {
+    @Override
+    public void execute() {
+        // Lógica para la entrada de datos de los jugadores
+        // ...
+    }
+}
